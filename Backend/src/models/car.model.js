85c0675f58e3676fd7a4c6be0460368d29aac01d
Avinash <avinashkumarsinha01@gmail.com>
2024-08-06 19:50:32 +0530

    update s_no

--- Backend/src/models/car.model.js
+++ Backend/src/models/car.model.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
 
 const CarSchema = new mongoose.Schema({
+    S_NO : {type: Number},
     carName: { type: String, required: true },
     manufacturingYear: { type: Number, required: true },
     price: { type: Number, required: true },
 });
-
+CarSchema.plugin(AutoIncrement, { inc_field: ' S_NO' });
 module.exports = mongoose.model('Car', CarSchema);
